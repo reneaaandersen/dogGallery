89e5dc985b5bfa557fb3f091373133736b6c6100
René <reand75@gmail.com>
2018-09-10 15:10:27 +0200

    Minor fixes

--- opgave.php
+++ opgave.php
@@ -70,7 +70,7 @@
 		<div class="modal-dialog modal-dialog-centered">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h4 class="modal-title">Modal Heading</h4>
+					<h4 class="modal-title text-capitalize">Title</h4>
 					<button type="button" class="close" data-dismiss="modal">&times;</button>
 				</div>
 				<div class="modal-body">
